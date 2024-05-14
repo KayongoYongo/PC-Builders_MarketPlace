1c852f8455adc2d33fe2e9546fe2d77b2e56ada7
KayongoYongo <sknsammie10@gmail.com>
2024-05-14 23:29:43 +0300

    modified the customers_app by putting the correct template route

--- pc_builders_marketplace/customers_app/views.py
+++ pc_builders_marketplace/customers_app/views.py
@@ -4,4 +4,4 @@ from customers_app.forms.sign_up_form import signUpForm
 # Create your views here.
 def sign_up_page(request):
     context = {'form': signUpForm()}
-    return render(request, 'pc_builders_marketplace_app/sign_up.html', context)
+    return render(request, 'customers_app/sign_up.html', context)
