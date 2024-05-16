455d41da6b2ba5cd1d61f62fa96abd8612174df3
KayongoYongo <sknsammie10@gmail.com>
2024-05-17 01:50:19 +0300

    added the urls for sign up and log in page

--- pc_builders_marketplace/customers_app/urls.py
+++ pc_builders_marketplace/customers_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import sign_up_page
+from . import views
 
 urlpatterns = [
-    path('sign_up', sign_up_page, name='sign_up_page'),
+    path("", views.home_page, name='home_page'),
+    path('sign_up', views.sign_up_page, name='sign_up_page'),
+    path('log_in', views.log_in_page, name='log_in_page'),
+    path('logout', views.logout_page, name='logout_page')
 ]
