5e709086621ebfc2d7e6adda96efaf1b336d57ed
KayongoYongo <sknsammie10@gmail.com>
2024-05-17 01:54:34 +0300

    added methods for rendering sign up, log in and index page

--- pc_builders_marketplace/customers_app/views.py
+++ pc_builders_marketplace/customers_app/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
 from customers_app.forms.sign_up_form import signUpForm
+from customers_app.forms.log_in_form import logInForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
-# Create your views here.
+# Define method for rendering the home page
+def home_page(request):
+    context = {'form': signUpForm()}
+    return render(request, 'customers_app/index.html', context)
+
+# Define method for rendering sign up page
 def sign_up_page(request):
     context = {'form': signUpForm()}
     return render(request, 'customers_app/sign_up.html', context)
+
+# Define method for rendering log in page
+def log_in_page(request):
+    context = {'form': logInForm()}
+    return render(request, 'customers_app/log_in.html', context)
+
+# Define method for rendering log out page
+def logout_page(request):
+    logout(request)
+    return redirect('home_page')
