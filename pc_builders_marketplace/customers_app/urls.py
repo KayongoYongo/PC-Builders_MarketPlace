d52a20a58fba1218261f2fc88834a907e8c9ba16
KayongoYongo <sknsammie10@gmail.com>
2024-05-20 16:22:48 +0300

    added the urls for sign up and log in

--- pc_builders_marketplace/customers_app/urls.py
+++ pc_builders_marketplace/customers_app/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path("", views.home_page, name='home_page'),
     path('sign-up', views.sign_up, name='sign_up'),
-    path('log-in', views.log_in_page, name='log_in_page'),
+    path('log-in', views.log_in, name='log_in'),
     path('logout', views.logout_page, name='logout_page')
 ]
