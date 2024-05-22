80e0c9ddfec1bee784e20aa3ccbff3acaa715e35
KayongoYongo <sknsammie10@gmail.com>
2024-05-22 21:18:15 +0300

    Added a url to work with deleting blogs

--- pc_builders_marketplace/blog_app/urls.py
+++ pc_builders_marketplace/blog_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.blog_list, name='blog_list'),
     path("create-blog", views.create_blog, name='create_blog'),
     path('view-blog/<int:id>', views.view_blog, name='view_blog'),
-    path('my-blogs/', views.view_my_blogs, name='view_my_blogs' )
+    path('my-blogs/', views.view_my_blogs, name='view_my_blogs'),
+    path('my-blogs/delete-blog/<int:id>/', views.delete_blog, name='delete_blog'),
 ]
