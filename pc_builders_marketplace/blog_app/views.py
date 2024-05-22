7f246a0352fc6045a817dcf575f19e9b949938b3
KayongoYongo <sknsammie10@gmail.com>
2024-05-22 23:14:23 +0300

    added the function for editing the views and added doc string comments to the function

--- pc_builders_marketplace/blog_app/views.py
+++ pc_builders_marketplace/blog_app/views.py
@@ -11,53 +11,100 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 def blog_list(request):
     """
-    This function returns all the blog posts
+    Description:
+        This function returns all the blog posts available in the database.
+
+    Args:
+        request: The HTTP request object that contains metadata about the request.
+
+    Return:
+        Renders a template with the blog data
     """
+
+    # Retireve all blog objects from the database and store it in the variable blog
     blogs = Blog.objects.all()
+
+    # Render the blog_list template with the blog data
     return render(request, 'blog_app/blog_list.html', {'blogs': blogs})
 
 def view_blog(request, id):
     """
-    This function enables a user to view a single blog post
+    Description:
+        This function enables a user to view a single blog post.
+
+    Args:
+        request: The HTTP request object that contains metadata about the request.
+
+    Return:
+        Renders a template with the blog data
     """
+
+    # Retrieve the blog object with the given ID, or return a 404 error if not found
     blog = get_object_or_404(Blog, id=id)
+
+    # Render the blog_detail template with the blog data
     return render(request, 'blog_app/blog_detail.html', {'blog': blog})
 
 # This makes sure that only a user that is logged in can create a blog
 @login_required
 def create_blog(request):
     """
-    This function creates a blog in Django
+    Description:
+        This function enables a user to create a single blog post.
+        On GET, the function enables a user to render the add_blog template.
+
+    Args:
+        request: The HTTP request object that contains metadata about the request.
+
+    Return:
+        On POST request, the function adds a blog post and redirects to the page
+        where a user created their individual blog post.
+        On GET request, the function renders the add_blog template.
     """
+
     if request.method == 'POST':
         # Instantiate the form with POST data
         form = BlogForm(request.POST)
 
+        # Validate the form data
         if form.is_valid():
+            # Retrieve cleaned data from the form
             title = form.cleaned_data['title']
             content = form.cleaned_data['content']
             status = form.cleaned_data['status']
             author = request.user
 
-            # Show the output information
-            print(title, content, author, status)
-
             # Create the blog post
             Blog.objects.create(title=title, content=content, status=status, author=author)
 
-            return redirect('blog_list')
+            # Redirect the user to the page showing their blogs
+            return redirect('view_my_blogs')
     else:
         form = BlogForm()
 
+    # Render the add blog template with the form data
     return render(request, 'blog_app/add_blog.html', {'form': form})
 
 @login_required
 def view_my_blogs(request):
     """
-    This function returns blogs created by the individual user
+    Description:
+        This function returns blogs created by the individual user.
+
+    Args:
+        request: The HTTP request object that contains metadata about the request.
+
+    Return:
+        The function renders the my_blog template
     """
+
+    # Retrieve the user metadata from the request object
     user = request.user
+
+    # Filter the blogs based on the author
     blogs = Blog.objects.filter(author=user)
+
+    # Render the my_blogs template with the blogs and the username
     return render(request, 'blog_app/my_blogs.html', {'blogs': blogs, 'username': user.username})
 
 @login_required
@@ -65,6 +112,7 @@ def delete_blog(request, id):
     """
     This function deletes a blog 
     """
+    # Retrieve the blog object with the given ID, or return a 404 error if not found
     blog = get_object_or_404(Blog, id=id)
 
     # Delete the blog
@@ -72,4 +120,49 @@ def delete_blog(request, id):
 
     # Send the user a message
     messages.success(request, "Blog post deleted successfully.")
+
+    # Redirect the user to the page showing their blogs
     return redirect('view_my_blogs')
+
+@login_required
+def update_blog(request, id):
+    """
+    This function handles updating an existing blog post.
+    It ensures that the user is logged in and then processes the update form.
+    """
+
+    # Retrieve the blog object with the given ID, or return a 404 error if not found
+    blog = get_object_or_404(Blog, id=id)
+
+    # Check if the request method is POST, indicating that the form has been submitted
+    if request.method == 'POST':
+        # Instantiate the form with the data submitted in the request
+        form = BlogForm(request.POST)
+
+        # Validate the form data
+        if form.is_valid():
+            # Retrieve cleaned data from the form
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            status = form.cleaned_data['status']
+
+            # Update the blog object with the new data
+            blog.title = title
+            blog.content = content
+            blog.status = status
+
+            # Save the updated blog object to the database
+            blog.save()
+
+            # Display a success message to the user
+            messages.success(request, "Blog post updated successfully")
+
+            # Redirect the user to the page showing their blogs
+            return redirect('view_my_blogs')
+        
+    else:
+        # If the request method is not POST, instantiate the form with the current blog instance
+        form = BlogForm(instance=blog)
+
+    # Render the edit blog template with the form and blog data
+    return render(request, 'blog_app/edit_blog.html', {'form': form, 'blog': blog})
