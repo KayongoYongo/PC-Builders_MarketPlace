1354c75c8819a6c63e2bca7120ba23e390ecb215
KayongoYongo <sknsammie10@gmail.com>
2024-05-22 20:22:51 +0300

    added a url for viewing a users individual blog

--- pc_builders_marketplace/blog_app/urls.py
+++ pc_builders_marketplace/blog_app/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path("", views.blog_list, name='blog_list'),
     path("create-blog", views.create_blog, name='create_blog'),
-    path('view-blog/<int:id>', views.view_blog, name='view_blog')
+    path('view-blog/<int:id>', views.view_blog, name='view_blog'),
+    path('my-blogs/', views.view_my_blogs, name='view_my_blogs' )
 ]
