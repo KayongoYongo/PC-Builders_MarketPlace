df601cdb75d0f1eb4e7536b50f324044ade5bce3
KayongoYongo <sknsammie10@gmail.com>
2024-05-28 20:28:06 +0300

    updated the content field to work with ck editor 5

--- pc_builders_marketplace/blog_app/models.py
+++ pc_builders_marketplace/blog_app/models.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.db import models
-from ckeditor.fields import RichTextField
+from django_ckeditor_5.fields import CKEditor5Field
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    content = RichTextField(blank=True, null=True)
+    content = CKEditor5Field(blank=True, null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
